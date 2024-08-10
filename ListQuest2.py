d7352208fee66a0eab6c85160ef204f7e1652d64
Manoel Martiniano <166198049+ManoelMartinianoNeto@users.noreply.github.com>
2024-08-09 23:57:59 -0300

    Create ListQuest2.py
    AVO1

---
+++ ListQuest2.py
@@ -0,0 +1,7 @@
+numero_1 = int(input('Digite o primeiro número: '))
+numero_2 = int(input('Digite o segundo número: '))
+
+if numero_1 > numero_2:
+    print(f'{numero_1} é maior que {numero_2}')
+else:
+    print(f'{numero_2} é maior que {numero_1}')
