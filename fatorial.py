cb5e336a0c53d70821a7dd6485395cc340f84e11
Manoel Martiniano <166198049+ManoelMartinianoNeto@users.noreply.github.com>
2024-10-04 23:14:31 -0300

    fatorial.py
    Escreva uma função que calcule o fatorial de um número inteiro
    não-negativo.

---
+++ fatorial.py
@@ -0,0 +1,10 @@
+def fatorial(n):
+    if n < 0:
+        raise ValueError("O número deve ser não-negativo.")
+    resultado = 1
+    for i in range(2, n + 1):
+        resultado *= i
+    return resultado
+
+numero = 5
+print(f"O fatorial de {numero} é {fatorial(numero)}.")
