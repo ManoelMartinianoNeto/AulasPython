ca25a7cdb5925a5551778aaa6e403c7b510e0edb
Manoel Martiniano <166198049+ManoelMartinianoNeto@users.noreply.github.com>
2024-10-04 23:18:03 -0300

    contar_palavras.py
     Crie uma função que receba uma string e retorne o número de palavras na
    string.

--- contar_palavras.py
+++ contar_palavras.py
@@ -1,8 +1,7 @@
 def contar_palavras(s):
     palavras = s.split()
     return len(palavras)
-
-# Exemplo de uso
+    
 string_exemplo = "Olá, como você está?"
 numero_de_palavras = contar_palavras(string_exemplo)
 print(f"Número de palavras: {numero_de_palavras}.")
